834510ec73946a4a2d2ccbbe2c7ae7ad32f2170f
Pawel <p.bartela@gmail.com>
2023-10-29 22:21:16 +0100

    Added two extra links to intentions and announcements. Corrected css in nav main-menu__list section - moved transition from li.visible to li element itself.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,42 @@ class Nav {
         this.navMenu = document.querySelector('#nav');
         this.body = document.querySelector('body');
 
-        // this.setBurgerBars();
+        this.menuItems = document.querySelectorAll('#main-menu__list li');
     }    
 
-    addEventsListeners() {
+    addEventsListeners = () => {
         this.burgerBtn.addEventListener('click', e => {
             this.navMenu.classList.toggle('nav--active');
 
             if(this.navMenu.classList.contains('nav--active')) {
                 this.burgerBars.classList.add('burger-menu__bars--active');
                 this.burgerOutline.classList.add('burger-menu--active');
+                
+                this.showMenuItems();
             } else {
+                this.hideMenuItems();
                 this.burgerBars.classList.remove('burger-menu__bars--active');
                 this.burgerOutline.classList.remove('burger-menu--active');
+
             }
 
         });
+    }
+
+    showMenuItems = () => {
+        let i=0;
+        this.menuItems.forEach(item => {
+            console.log(item.firstChild);
+            item.classList.add('visible');
+            item.style.transitionDelay = (i += 0.1) + 's';
+        });
+    }
+
+    hideMenuItems = () => {
+        this.menuItems.forEach(item => {
+            item.classList.remove('visible');
+            item.style = '';
+        })
     }
 }
 
